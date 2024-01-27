23319ea361fbddcbd0c26bf8329bcf175392be10
abchiyi <meme_me2019@outlook.com>
2024-01-27 16:24:40 +0800

    change

--- src/Radio.cpp
+++ src/Radio.cpp
@@ -10,7 +10,6 @@
 #define PASSWORD "Slave_1_Password"
 
 esp_now_peer_info peerInfo;
-// esp_now_recv_cb_t *onRecvCb;
 
 int CONNECT_TIMEOUT = 500;              // ms // 连接同步等待时间
 int ConnectedTimeOut = CONNECT_TIMEOUT; // ms
@@ -19,6 +18,8 @@ bool PairRuning = false;
 bool IsPaired = false;
 int Send_gap_ms = 0;
 void *RecvData;
+
+int *CHANNEL; // 通讯频道
 
 // 返回mac地址字符串
 String parseMac(const uint8_t *mac)
@@ -29,49 +30,49 @@ String parseMac(const uint8_t *mac)
   return String(macStr);
 }
 
+// 接收回调，在这里执行配对程序&接收数据/发送数据
 void onRecvCb(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
-
-  esp_err_t a = esp_now_send(peerInfo.peer_addr, incomingData, len);
-  ESP_LOGI("Radio", "Controller mac : %s, Send data %s",
-           parseMac(mac), a == ESP_OK ? "success" : " fail");
-  // if not pair
-
-  if (esp_now_add_peer(&peerInfo) == ESP_OK) // 配对
+  if (IsPaired)
   {
-    // 记录主机mac地址后向主机发送配对信息
-    WiFi.mode(WIFI_STA);
-    esp_err_t result = esp_now_send(mac,
-                                    (const uint8_t *)WiFi.softAPSSID().c_str(), sizeof((const uint8_t *)WiFi.softAPSSID().c_str()));
-    Serial.print("Send Status: ");
-    if (result == ESP_OK)
+    esp_err_t a = esp_now_send(peerInfo.peer_addr, incomingData, len);
+    ESP_LOGI("Radio", "Controller mac : %s, Send data %s",
+             parseMac(mac), a == ESP_OK ? "success" : " fail");
+  }
+  else // if not pair
+  {
+    if (esp_now_add_peer(&peerInfo) == ESP_OK) // 配对
     {
-      Serial.println("send ok");
-      esp_now_unregister_recv_cb() == ESP_OK ? Serial.println("unreg peer ok") : Serial.println("unreg peer fail");
+      // 记录主机mac地址后向主机发送配对信息
+      WiFi.mode(WIFI_STA);
+      esp_err_t result = esp_now_send(mac,
+                                      (const uint8_t *)WiFi.softAPSSID().c_str(), sizeof((const uint8_t *)WiFi.softAPSSID().c_str()));
+      Serial.print("Send Status: ");
+      if (result == ESP_OK)
+      {
+        Serial.println("send ok");
+        esp_now_unregister_recv_cb() == ESP_OK ? Serial.println("unreg peer ok") : Serial.println("unreg peer fail");
+      }
+      else
+      {
+        Serial.println("Send fail");
+      }
     }
     else
     {
-      Serial.println("Send fail");
+      Serial.println("Peer fail");
     }
-  }
-  else
-  {
-    Serial.println("Peer fail");
   }
 }
 
-/**
- * @brief 启动 esp_now 通讯
- */
-void Radio::begin(const char *ssid, uint8_t channel)
+void EspNowInit()
 {
-  controller = &peerInfo; // 设置配对对象
 
+  // wifi set
   WiFi.mode(WIFI_AP);
-  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE); // 设置通讯频道
+  esp_wifi_set_channel(*CHANNEL, WIFI_SECOND_CHAN_NONE);
 
-  esp_now_init(); // 初始化 esp_now
-  if (WiFi.softAP(SSID, PASSWORD, channel, 0))
+  if (WiFi.softAP(SSID, PASSWORD, *CHANNEL, 0))
   {
     ESP_LOGI(TAG, "AP Config Success. Broadcasting with AP %s", String(SSID).c_str());
     ESP_LOGI(TAG, "AP soft mac : %s, Channel : %u", WiFi.softAPmacAddress(), WiFi.channel());
@@ -81,4 +82,37 @@ void Radio::begin(const char *ssid, uint8_t channel)
   {
     Serial.println("AP Config failed.");
   }
+
+  // esp_now_set
+  static int counter = 0;
+  esp_now_deinit(); // 重置 esp_now
+  if (esp_now_init() == ESP_OK)
+  {
+    ESP_LOGI(TAG, "ESP NOW init success");
+    esp_err_t refStatus = esp_now_register_recv_cb(onRecvCb);
+    refStatus == ESP_OK
+        ? ESP_LOGI(TAG, "Register recv cb success")
+        : ESP_LOGE(TAG, "Register recv cb fail");
+    counter = 0;
+  }
+  else
+  {
+    counter++;
+    counter <= 5
+        ? ESP_LOGE(TAG, "ESP NOW init fail, Try again...")
+        : ESP_LOGE(TAG, "ESP NOW init fail, Maximum depth, restart...");
+    delay(10);
+    counter <= 5 ? EspNowInit() : ESP.restart();
+  }
+}
+
+/**
+ * @brief 启动 esp_now 通讯
+ */
+void Radio::begin(const char *ssid, uint8_t channel)
+{
+  controller = &peerInfo; // 设置配对对象
+  Channel = channel;
+  CHANNEL = (int *)&Channel;
+  esp_now_init();
 }
--- src/Radio.h
+++ src/Radio.h
@@ -9,6 +9,6 @@ private:
   /* data */
 public:
   esp_now_peer_info *controller; // 无线控制器的配对信息
-  int channel = 1;               // XXX 需要可自定义的通道
+  int Channel = 1;               // 通讯频道
   void begin(const char *ssid, uint8_t channel);
 };
